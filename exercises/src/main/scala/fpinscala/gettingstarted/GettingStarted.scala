2e1a800f5af4f2e91c4eb2b52fe0fed9f3970de0
Corina Usher <Corina.Usher@barclayscapital.com>
2015-02-18 22:32:34 +0000

    Exercises 2.4, 2.5 (Currying, uncurrying and composition)

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -165,13 +165,13 @@ object PolymorphicFunctions {
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
   def curry[A, B, C](f: (A, B) => C): A => (B => C) =
-    ???
+    (a: A) => (b: B) => f(a, b)
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
   def uncurry[A, B, C](f: A => B => C): (A, B) => C =
-    ???
+    (a: A, b: B) =>  f(a)(b)
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -182,9 +182,8 @@ object PolymorphicFunctions {
   we say that they are _isomorphic_ ("iso" = same; "morphe" = shape, form),
   a term we inherit from category theory.
   */
-
+  // CU: cheat answer is g andThen f
   // Exercise 5: Implement `compose`
 
-  def compose[A, B, C](f: B => C, g: A => B): A => C =
-    ???
+  def compose[A, B, C](f: B => C, g: A => B): A => C = (a: A) => f(g(a))
 }
