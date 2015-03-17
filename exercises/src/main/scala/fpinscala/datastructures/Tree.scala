a96914a7d55e4815a304ec838fc080f7d56122e7
Corina Usher <Corina.Usher@barclayscapital.com>
2015-03-17 15:20:09 +0000

    Exercise 3.25: implement size method on tree to count number of nodes in tree

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -1,13 +1,14 @@
 package fpinscala.datastructures
 
 sealed trait Tree[+A]
+
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
+case object TreeHelper {
 
-object Tree {
-
-
-
-
+  def size[A](tree: Tree[A], acc: Int): Int = tree match {
+    case a: Branch[A] => size(a.left, size(a.right, acc + 1))
+    case a: Leaf[A] => acc + 1
+  }
 }
---
+++ exercises/src/test/scala/gettingstarted/TreeTest.scala
@@ -0,0 +1,14 @@
+package gettingstarted
+
+import fpinscala.datastructures.{TreeHelper, Branch, Leaf, Tree}
+
+class TreeTest extends org.specs2.mutable.Specification{
+  "size" should {
+    "return the number of nodes (leaves and branches) in the tree" in {
+      TreeHelper.size( Leaf("a"), 0) must beEqualTo(1)
+      TreeHelper.size( Branch(Branch(Leaf("a"), Leaf("b")), Branch(Leaf("c"), Leaf("d"))), 0) must beEqualTo(7)
+      TreeHelper.size( Branch(Branch(Leaf("a"), Leaf("b")), Leaf("d")), 0) must beEqualTo(5)
+      TreeHelper.size( Branch(Branch(Leaf("a"), Leaf("b")), Leaf("d")), 0) must beEqualTo(5)
+    }
+  }
+}
