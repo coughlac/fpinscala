435d0638f78450c17ba2c50cb19b5ad6c9c32174
Corina Usher <Corina.Usher@barclayscapital.com>
2015-03-24 11:29:45 +0000

    Exercise 3.26 - Implemented maximum and tidied up size

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -6,8 +6,17 @@ case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 case object TreeHelper {
-  def size[A](tree: Tree[A], acc: Int): Int = tree match {
+  def size[A](tree: Tree[A]): Int = size(tree, 0)
+
+  private def size[A](tree: Tree[A], acc: Int): Int = tree match {
     case a: Branch[A] => size(a.left, size(a.right, acc + 1))
     case a: Leaf[A] => acc + 1
+  }
+
+  def maximum(tree: Tree[Int]): Int = maximum(tree, 0)
+
+  private def maximum(tree: Tree[Int], result: Int): Int = tree match {
+    case a: Branch[Int] => maximum(a.left, maximum(a.right, result))
+    case a: Leaf[Int] => result.max(a.value)
   }
 }
--- exercises/src/test/scala/gettingstarted/TreeTest.scala
+++ exercises/src/test/scala/gettingstarted/TreeTest.scala
@@ -5,10 +5,19 @@ import fpinscala.datastructures.{TreeHelper, Branch, Leaf}
 class TreeTest extends org.specs2.mutable.Specification{
   "size" should {
     "return the number of nodes (leaves and branches) in the tree" in {
-      TreeHelper.size( Leaf("a"), 0) must beEqualTo(1)
-      TreeHelper.size( Branch(Branch(Leaf("a"), Leaf("b")), Branch(Leaf("c"), Leaf("d"))), 0) must beEqualTo(7)
-      TreeHelper.size( Branch(Branch(Leaf("a"), Leaf("b")), Leaf("d")), 0) must beEqualTo(5)
-      TreeHelper.size( Branch(Branch(Leaf("a"), Leaf("b")), Leaf("d")), 0) must beEqualTo(5)
+      TreeHelper.size( Leaf("a")) must beEqualTo(1)
+      TreeHelper.size( Branch(Branch(Leaf("a"), Leaf("b")), Branch(Leaf("c"), Leaf("d")))) must beEqualTo(7)
+      TreeHelper.size( Branch(Branch(Leaf("a"), Leaf("b")), Leaf("d"))) must beEqualTo(5)
+      TreeHelper.size( Branch(Branch(Leaf("a"), Leaf("b")), Leaf("d"))) must beEqualTo(5)
+    }
+  }
+
+  "maximum" should {
+    "return the maximum element of nodes (leaves and branches) in the tree of ints" in {
+      TreeHelper.maximum( Leaf(2)) must beEqualTo(2)
+      TreeHelper.maximum( Branch(Branch(Leaf(30), Leaf(50)), Branch(Leaf(70), Leaf(90)))) must beEqualTo(90)
+      TreeHelper.maximum( Branch(Branch(Leaf(4), Leaf(9)), Leaf(3))) must beEqualTo(9)
+      TreeHelper.maximum( Branch(Branch(Leaf(4), Leaf(5)), Leaf(1))) must beEqualTo(5)
     }
   }
 }
