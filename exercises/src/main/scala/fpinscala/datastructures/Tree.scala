d176ac484804da440ad080aa9ddf3a00133b57b1
Corina Usher <Corina.Usher@barclayscapital.com>
2015-03-27 08:07:11 +0000

    3.30 using fold in an oo way

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -1,10 +1,16 @@
 package fpinscala.datastructures
 
-sealed trait Tree[+A]
+sealed trait Tree[+A] {
+  def fold[B](l: A => B)(b: (B, B) => B): B
+}
 
-case class Leaf[A](value: A) extends Tree[A]
+case class Leaf[A](value: A) extends Tree[A] {
+  def fold[B](l: (A) => B)(b: (B, B) => B): B = l(value)
+}
 
-case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A] {
+  def fold[B](l: (A) => B)(b: (B, B) => B): B = b(left.fold(l)(b), right.fold(l)(b))
+}
 
 case object TreeHelper {
   private def fold[A, B](t: Tree[A])(l: A => B)(b: (B, B) => B): B = t match {
@@ -16,7 +22,8 @@ case object TreeHelper {
 
   def map[A, B](tree: Tree[A])(f: A => B): Tree[B] = fold[A, Tree[B]](tree)(a => Leaf(f(a)))(Branch(_, _))
 
-  def size[A](tree: Tree[A]): Int = fold[A, Int](tree)(_ => 1)((l, r) => l + r + 1)
+  def size[A](tree: Tree[A]): Int = tree.fold[Int](_ => 1)((l, r) => l + r + 1)
+  //fold[A, Int](tree)(_ => 1)((l, r) => l + r + 1)
 
   def maximum(tree: Tree[Int]): Int = fold[Int, Int](tree)(a => a)(_ max _)
 }
