3bb42c83438d54eaac6adfb5a9d368d6ff8028e8
Corina Usher <Corina.Usher@barclayscapital.com>
2015-05-31 14:36:15 +0100

    5.3 takeWhile

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -1,5 +1,5 @@
 package fpinscala.laziness
-import Stream._
+
 trait Stream[+A] {
 
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
@@ -16,6 +16,7 @@ trait Stream[+A] {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
+
   def take(n: Int): Stream[A] = this match {
     case Empty                ⇒ Empty
     case _          if n == 0 ⇒ Empty
@@ -28,7 +29,20 @@ trait Stream[+A] {
     case Cons(h, t)           ⇒ t().drop(n-1)
   }
 
-  def takeWhile(p: A => Boolean): Stream[A] = sys.error("todo")
+  def takeWhile(p: A => Boolean): Stream[A] = {
+    this match {
+      case Empty ⇒ Empty
+      case Cons(h, t) if !p(h()) ⇒ Empty
+      case Cons(h, t) if p(h()) ⇒ Cons(h, () ⇒ t().takeWhile(p))
+    }
+  }
+//      foldRight(empty: Stream[A])((head, acc) ⇒ {
+//        if (p(head))
+//          Cons(() ⇒ head, () ⇒ acc)
+//        else acc
+//      }
+//    )
+
 
   def forAll(p: A => Boolean): Boolean = sys.error("todo")
 
--- exercises/src/test/scala/gettingstarted/LazyStreamTest.scala
+++ exercises/src/test/scala/gettingstarted/LazyStreamTest.scala
@@ -8,7 +8,10 @@ class LazyStreamTest extends org.specs2.mutable.Specification {
       Empty.toList should beEqualTo(Nil)
     }
     "return the elements of the stream as a list if not empty" in {
-      Cons(() ⇒ "A",  () ⇒ Cons(() ⇒ "B", () ⇒ Cons(() ⇒ "C", () ⇒ Empty))).toList should beEqualTo("A" :: "B" :: "C" :: Nil)
+      val actualStream = Cons(() ⇒ "A", () ⇒ Cons(() ⇒ "B", () ⇒ Cons(() ⇒ "C", () ⇒ Empty)))
+      val expectedList = "A" :: "B" :: "C" :: Nil
+
+      actualStream.toList should beEqualTo(expectedList)
     }
   }
 
@@ -18,19 +21,21 @@ class LazyStreamTest extends org.specs2.mutable.Specification {
     }
 
     "return an Empty Stream if n was 0" in {
-      val actualStream = Cons(() ⇒ "A",  () ⇒ Cons(() ⇒ "B", () ⇒ Cons(() ⇒ "C", () ⇒ Empty)))
+      val actualStream = Cons(() ⇒ "A", () ⇒ Cons(() ⇒ "B", () ⇒ Cons(() ⇒ "C", () ⇒ Empty)))
       actualStream.take(0) must beEqualTo(Empty)
     }
 
     "take n elements from the head of the stream" in {
-      val actualStream = Cons(() ⇒ "A",  () ⇒ Cons(() ⇒ "B", () ⇒ Cons(() ⇒ "C", () ⇒ Empty)))
-      val expectedStream = Cons(() ⇒ "A",  () ⇒ Cons(() ⇒ "B", ()⇒ Empty))
+      val actualStream = Cons(() ⇒ "A", () ⇒ Cons(() ⇒ "B", () ⇒ Cons(() ⇒ "C", () ⇒ Empty)))
+      val expectedStream = Cons(() ⇒ "A", () ⇒ Cons(() ⇒ "B", () ⇒ Empty))
+
       actualStream.take(2).toList must beEqualTo(expectedStream.toList)
     }
 
     "return the original stream if n is greater than the size of the original stream" in {
-      val actualStream = Cons(() ⇒ "A",  () ⇒ Cons(() ⇒ "B", () ⇒ Cons(() ⇒ "C", () ⇒ Empty)))
+      val actualStream = Cons(() ⇒ "A", () ⇒ Cons(() ⇒ "B", () ⇒ Cons(() ⇒ "C", () ⇒ Empty)))
       val expectedStream = actualStream
+
       actualStream.take(4).toList must beEqualTo(expectedStream.toList)
     }
   }
@@ -41,14 +46,32 @@ class LazyStreamTest extends org.specs2.mutable.Specification {
     }
 
     "not change the original stream if n was 0" in {
-      val actualStream = Cons(() ⇒ "A",  () ⇒ Cons(() ⇒ "B", () ⇒ Cons(() ⇒ "C", () ⇒ Empty)))
+      val actualStream = Cons(() ⇒ "A", () ⇒ Cons(() ⇒ "B", () ⇒ Cons(() ⇒ "C", () ⇒ Empty)))
       actualStream.drop(0) must beEqualTo(actualStream)
     }
 
     "take n elements from the head of the stream" in {
-      val actualStream = Cons(() ⇒ "A",  () ⇒ Cons(() ⇒ "B", () ⇒ Cons(() ⇒ "C", () ⇒ Empty)))
+      val actualStream = Cons(() ⇒ "A", () ⇒ Cons(() ⇒ "B", () ⇒ Cons(() ⇒ "C", () ⇒ Empty)))
       val expectedStream = Cons(() ⇒ "C", () ⇒ Empty)
+
       actualStream.drop(2).toList must beEqualTo(expectedStream.toList)
+    }
+  }
+
+  "takeWhile" should {
+    "not change the original stream if it is empty" in {
+      Empty.takeWhile(_.equals("A")) must beEqualTo(Empty)
+    }
+
+    "return an Empty Stream if predicate was never met" in {
+      val actualStream = Cons(() ⇒ "A", () ⇒ Cons(() ⇒ "B", () ⇒ Cons(() ⇒ "C", () ⇒ Empty)))
+      actualStream.takeWhile(_.equals(1)) must beEqualTo(Empty)
+    }
+
+    "returns elements of a Stream just until the predicate fails" in {
+      val actualStream = Cons(() ⇒ "A", () ⇒ Cons(() ⇒ "B", () ⇒ Cons(() ⇒ "C", () ⇒ Cons(() ⇒ "D", () ⇒ Empty))))
+      val expectedStream = Cons(() ⇒ "A", () ⇒ Cons(() ⇒ "B", () ⇒ Empty))
+      actualStream.takeWhile(!_.equals("C")).toList must beEqualTo(expectedStream.toList)
     }
   }
 }
